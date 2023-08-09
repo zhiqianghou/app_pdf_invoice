8a5769e1d747f3a9797574e161f57eff6a38029a
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-08-09 17:49:48 -0500

    Add header, total price, contributor and logo

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ for filepath in filepaths:
 	columns = list(df.columns)
 	columns = [item.replace("_", " ").title() for item in columns]
 
+	# Add Header
 	pdf.set_font(family="Times", size=10, style="B")
 	pdf.set_text_color(80, 80, 80)
 	pdf.cell(w=30, h=8, txt=str(columns[0]), border=1)
@@ -32,6 +33,7 @@ for filepath in filepaths:
 	pdf.cell(w=30, h=8, txt=str(columns[3]), border=1)
 	pdf.cell(w=30, h=8, txt=str(columns[4]), border=1, ln=1)
 
+	# Add rows to the table
 	for index, row in df.iterrows():
 		pdf.set_font(family="Times", size=10)
 		pdf.set_text_color(80, 80, 80)
@@ -40,6 +42,25 @@ for filepath in filepaths:
 		pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
 		pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
 		pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+	# Add total sum
+	total_sum = df["total_price"].sum()
+	pdf.set_font(family="Times", size=10)
+	pdf.set_text_color(80, 80, 80)
+	pdf.cell(w=30, h=8, txt="", border=1)
+	pdf.cell(w=60, h=8, txt="", border=1)
+	pdf.cell(w=40, h=8, txt="", border=1)
+	pdf.cell(w=30, h=8, txt="", border=1)
+	pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+	# Add total sum sentence
+	pdf.set_font(family="Times", size=10, style="B")
+	pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+	# Add author name and logo
+	pdf.set_font(family="Times", size=10, style="B")
+	pdf.cell(w=20, h=8, txt=f"Created by HOUZH")
+	pdf.image("pythonhow.png", x=42, w=8)
 
 
 
